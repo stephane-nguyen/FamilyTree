0be82067ba012011abca063783e11c206d670754
stephane-nguyen <stepdh01@gmail.com>
2023-11-10 15:54:44 +0100

    add navbar

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<nav>
+	<a href="/">Home</a>
+	<a href="/about">About</a>
+	<a href="/table">Table</a>
+	<a class="nav-right-side" href="/login">Login</a>
+</nav>
+
+
+<style>
+
+	nav {
+		background-color: #333; 
+		padding: 10px; 
+		display: flex; /* Use flexbox to control positioning */
+		justify-content: space-between; /* Push elements to opposite ends */
+	}
+
+	nav a {
+		text-decoration: none; /* Remove underline from links */
+		color: #fff; 
+		margin: 0 10px; 
+	}
+
+	nav a:hover {
+		text-decoration: underline; 
+	}
+
+	.nav-right-side {
+		margin-left: auto;
+	}
+
+</style>
+
+<slot />
