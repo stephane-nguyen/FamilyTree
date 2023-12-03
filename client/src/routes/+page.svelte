bced55ac97342ea7a5757681d463aa4af561ff63
Stephane NGUYEN <stepdh01@gmail.com>
2023-12-03 02:34:18 +0100

    Rewrite homepage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto p-4 text-center">
+    <h1 class="text-4xl font-bold mb-6">Welcome to the Family Tree Application!</h1>
+    <p class="text-lg mb-8">
+        Unearth the past, cherish the present, and shape the future. 
+        Start tracing your roots today to preserve your family's history, one branch at a time.
+    </p>
+    <a 
+        href="/members" 
+        class="bg-zinc-400 hover:bg-zinc-500 text-white font-bold py-2 px-4 rounded-full inline-block">
+        Get Started
+    </a>
+</div>
