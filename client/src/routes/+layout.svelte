e1ec78675b075e6ef4371a6d959aa8761e77e393
Stephane NGUYEN <stepdh01@gmail.com>
2023-12-03 01:14:37 +0100

    Remove tailwind cdn, add dependencies instead

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,13 +1,19 @@
-<nav class="flex items-center justify-between bg-gray-200 p-4">
+<script>
+	import "../app.pcss";
+</script>
+
+<nav class="flex items-center justify-between bg-zinc-300 p-4">
 	<div class="flex items-center space-x-4">
-	  <a href="/" class="text-gray-800 hover:text-gray-600">Home</a>
-	  <a href="/about" class="text-gray-800 hover:text-gray-600">About</a>
-	  <a href="/members" class="text-gray-800 hover:text-gray-600">Table</a>
+		<a href="/" class="text-gray-800 hover:text-gray-600">Home</a>
+		<a href="/about" class="text-gray-800 hover:text-gray-600">About</a>
+		<a href="/members" class="text-gray-800 hover:text-gray-600">Table</a>
 	</div>
-	
+
 	<div>
-		<a href="/login" class="px-4 py-2 text-gray-800 hover:text-gray-600">Login</a>
+		<a href="/login" class="px-4 py-2 text-gray-800 hover:text-gray-600"
+			>Login</a
+		>
 	</div>
 </nav>
-  
+
 <slot />
