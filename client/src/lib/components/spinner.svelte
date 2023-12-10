1bb6daf371ce06ca91f39a8dff08325deff631ce
stephane-nguyen <stepdh01@gmail.com>
2023-12-10 13:21:11 +0100

    Add spinner

---
+++ client/src/lib/components/spinner.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { Circle3 } from "svelte-loading-spinners";
+</script>
+
+<div class="flex justify-center" style="height: auto;">
+  <Circle3 size="60" unit="px" duration="1s" />
+</div>
